ffc7e26399594544b5cad6ab1a7035c003d2359c
Kirsley Chennen <kchennen@unistra.fr>
2023-03-24 16:52:57 +0100

    📦️ - Add Flask run

--- mistic/cli.py
+++ mistic/cli.py
@@ -26,8 +26,27 @@ def cli():
               help='Server port. Default = {p}.'.format(p=configs['FLASK_APP_PORT']))
 @click_log.simple_verbosity_option(logger)
 def run(host, port):
-    """Run webapp server"""
+    """Run webapp via Uvicorn"""
     wsgi.wsgi(host=host, port=port)
+
+    end_time = time.time()
+    click.echo('Done in {}s\n'.format(round(number=(end_time - start_time), ndigits=3)))
+
+
+@cli.command()
+@click.option('--host', '-h',
+              type=str,
+              default=configs['FLASK_APP_HOST'],
+              required=False,
+              help='Host server name. Default = \'{h}\'.'.format(h=configs['FLASK_APP_HOST']))
+@click.option('--port', '-p',
+              type=int,
+              default=configs['FLASK_APP_PORT'],
+              help='Server port. Default = {p}.'.format(p=configs['FLASK_APP_PORT']))
+@click_log.simple_verbosity_option(logger)
+def flask(host, port):
+    """Run webapp via Flask"""
+    wsgi.run(host=host, port=port)
 
     end_time = time.time()
     click.echo('Done in {}s\n'.format(round(number=(end_time - start_time), ndigits=3)))
